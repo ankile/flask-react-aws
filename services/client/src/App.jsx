aed17e096629b2b2ea77feb37fa1b2bf408ae209
ankile <lars.ankile@gmail.com>
2021-01-09 15:09:10 +0100

    Small change to test CI

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -197,7 +197,7 @@ class App extends Component {
                     path="/"
                     render={() => (
                       <div>
-                        <h1 className="title is-1">Users</h1>
+                        <h1 className="title is-1">All users</h1>
                         <hr />
                         <br />
                         {this.isAuthenticated() && (
