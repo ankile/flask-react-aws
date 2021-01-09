fcba41ff03d015b28478592f3dcf5d466f0ff60c
ankile <lars.ankile@gmail.com>
2021-01-09 16:45:56 +0100

    Test env vars

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -197,7 +197,7 @@ class App extends Component {
                     path="/"
                     render={() => (
                       <div>
-                        <h1 className="title is-1">All users</h1>
+                        <h1 className="title is-1">All users (herro)</h1>
                         <hr />
                         <br />
                         {this.isAuthenticated() && (
